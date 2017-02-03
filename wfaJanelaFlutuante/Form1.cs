67aebfeb0662b91e069c8b1403fe0fcd84d3c198
José Júnior <junior_dariana@hotmail.com>
2017-02-02 23:20:35 -0200

    first commit

---
+++ wfaJanelaFlutuante/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfaJanelaFlutuante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point p = new Point(0, 370);
+            this.DesktopLocation = p;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point p = new Point(this.DesktopLocation.X + 1, this.DesktopLocation.Y);
+            this.DesktopLocation = p;
+            if(p.X==550)
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
